0d63aa164f9558a8bc9581ab0cf535e0cce38777
Dalton Durst <dalton@ubports.com>
2019-12-11 21:23:52 -0600

    TopLevelWindowModel: Don't refocus closed Window
    Prior to this, clearing rootFocus, closing the previously focused
    Window, then setting rootFocus would cause a use-after-delete, resulting
    in a segfault at best or undefined behavior at worst.
    
    Fixes https://github.com/ubports/unity8/issues/234

--- plugins/WindowManager/TopLevelWindowModel.cpp
+++ plugins/WindowManager/TopLevelWindowModel.cpp
@@ -38,7 +38,8 @@ Q_LOGGING_CATEGORY(TOPLEVELWINDOWMODEL, "toplevelwindowmodel", QtInfoMsg)
 namespace unityapi = unity::shell::application;
 
 TopLevelWindowModel::TopLevelWindowModel()
-    : m_nullWindow(createWindow(nullptr))
+    : m_nullWindow(createWindow(nullptr)),
+      m_surfaceManagerBusy(false)
 {
     connect(m_nullWindow, &Window::focusedChanged, this, [this] {
         Q_EMIT rootFocusChanged();
@@ -439,6 +440,10 @@ void TopLevelWindowModel::removeAt(int index)
         setFocusedWindow(nullptr);
     }
 
+    if (m_previousWindow == window) {
+        m_previousWindow = nullptr;
+    }
+
     if (m_closingAllApps) {
         if (m_windowModel.isEmpty()) {
             Q_EMIT closedAllWindows();
@@ -762,6 +767,9 @@ void TopLevelWindowModel::setRootFocus(bool focus)
         if (m_previousWindow && !m_previousWindow->focused() && !m_pendingActivation &&
             m_nullWindow == m_focusedWindow && m_previousWindow != m_nullWindow) {
             m_previousWindow->activate();
+        } else if (!m_pendingActivation) {
+            // The previous window does not exist any more, focus top window.
+            activateTopMostWindowWithoutId(-1);
         }
     } else {
         if (!m_nullWindow->focused()) {
